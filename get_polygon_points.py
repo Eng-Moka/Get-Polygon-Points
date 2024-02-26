8bc31ba538f07224621c0e95339e0dec7809fab9
Mohamed Mokashifi <132586649+Eng-Moka@users.noreply.github.com>
2024-02-26 10:38:27 +0300

    Update get_polygon_points.py

--- get_polygon_points.py
+++ get_polygon_points.py
@@ -1,14 +1,14 @@
 import os
-import geopandas as gpd
 import pandas as pd
+import geopandas as gpd
 from shapely.geometry import Polygon
 from shapely.geometry import Point
 
 
-INDEX_ID_FEILD = 'index_ID'  # Point index_ID field name 
+INDEX_ID_FIELD = 'index_ID'  # Point index_ID field name 
 NEW_ID_FIELD = 'Point_ID'  # field name for the NEW Point_ID (shifted_id)
 
-def get_shift_value(points_gpf, index_ID = INDEX_ID_FEILD ):
+def get_shift_value(points_gpf, index_ID = INDEX_ID_FIELD ):
     """
     Calculates the shift value based on the maximum y-coordinate point ID.
 
@@ -20,7 +20,7 @@ def get_shift_value(points_gpf, index_ID = INDEX_ID_FEILD ):
         int: The shift value.
     """
     max_y = points_gpf.geometry.y.max()
-    max_y_point_id = points_gpf.loc[points_gpf.geometry.y == max_y, INDEX_ID_FEILD].values[0]
+    max_y_point_id = points_gpf.loc[points_gpf.geometry.y == max_y, index_ID].values[0]
     shift_value = abs(max_y_point_id - 1)
     return shift_value
 
@@ -54,29 +54,31 @@ def get_shifted_points_gdf(points_gdf, shift_value):
         GeoDataFrame: The GeoDataFrame with shifted IDs.
     """
     points_num = len(points_gdf)
-    points_gdf[NEW_ID_FIELD] = [get_shifted_id(row[INDEX_ID_FEILD], shift_value, points_num) for _, row in points_gdf.iterrows()]
-    points_gdf[NEW_ID_FIELD] = points_gdf[NEW_ID_FIELD].astype('int64')  # Convert the new column to long integer
-    points_gdf.drop(columns=[INDEX_ID_FEILD], inplace=True)  # Delete the INDEX_ID_FEILD column
+    points_gdf[NEW_ID_FIELD] = [get_shifted_id(row[INDEX_ID_FIELD], shift_value, points_num) for _, row in points_gdf.iterrows()]
+    # Convert the new column to long integer
+    # unnecessary but just in case you use the points id for map series in ArcGIS Pro
+    points_gdf[NEW_ID_FIELD] = points_gdf[NEW_ID_FIELD].astype('int64')  
+    points_gdf.drop(columns=[INDEX_ID_FIELD], inplace=True)  # Delete the INDEX_ID_FIELD column
     return points_gdf
 
 
-def get_polygon_points(polygon_gpd):
+def get_polygon_points(polygon_gdf):
     """
     Extracts points from the exterior of each polygon in a GeoDataFrame
     and ensures the sequential numbering of the points starts at the point with the maximum Y coordinate.
 
     Parameters:
-        polygon_gpd (GeoDataFrame): A GeoDataFrame containing polygons.
+        polygon_gdf (GeoDataFrame): A GeoDataFrame containing polygons.
 
     Returns:
         GeoDataFrame: A GeoDataFrame containing points extracted from the polygons.
     """
     shifted_points_list = []  #create an empty list for all Polygon's points
     #loop  through all features in the input geodataframe
-    for _ , row in polygon_gpd.iterrows():
+    for _ , row in polygon_gdf.iterrows():
         #check if  geometry is Polygon or MultiPolygon
         if not isinstance(row['geometry'], Polygon):
-            print(f"Geometries with ID {row[POLYGON_ID_FEILD]} are not a Polygon.")
+            print(f"Geometries with ID {row[POLYGON_ID_FIELD]} are not a Polygon.")
             continue
 
         points_list = []  #create an empty list for current Polygon points
@@ -85,8 +87,8 @@ def get_polygon_points(polygon_gpd):
             point_geometry = Point(coord) #shapely point geometry
             # Create point data including index ID, polygon ID, and geometry
             point_data = {
-                          INDEX_ID_FEILD: index,
-                          POLYGON_ID_FEILD: row[POLYGON_ID_FEILD],
+                          INDEX_ID_FIELD : index,
+                          POLYGON_ID_FIELD: row[POLYGON_ID_FIELD],
                           'geometry': point_geometry
                           }
             #add current point data to the total list of points for current Polygon
@@ -96,7 +98,7 @@ def get_polygon_points(polygon_gpd):
         points_gdf = gpd.GeoDataFrame(points_list)  
         #get shift  value for current Polygon
         shift_value = get_shift_value(points_gdf) 
-        # Get points GeoDataFrame for the current Polygon after applying the shift value to the INDEX_ID_FEILD
+        # Get points GeoDataFrame for the current Polygon after applying the shift value to the INDEX_ID_FIELD
         shifted_points_gdf = get_shifted_points_gdf(points_gdf, shift_value) 
         #add to the total list of all Polygon's points
         shifted_points_list.append(shifted_points_gdf)  
@@ -104,8 +106,8 @@ def get_polygon_points(polygon_gpd):
     if shifted_points_list:
         # concatenad all point geodataframes in the shifted_points_list
         merged_shifted_points_gdf = gpd.GeoDataFrame(pd.concat(shifted_points_list, ignore_index=True))
-        # Set the coordinate system to be same as the polygon coordinate system
-        merged_shifted_points_gdf.crs = polygon_gpd.crs 
+        # Set the coordinate system to be same as polygon coordinate system
+        merged_shifted_points_gdf.crs = polygon_gdf.crs 
 
         return merged_shifted_points_gdf
     else:
@@ -115,17 +117,17 @@ def get_polygon_points(polygon_gpd):
 
 if '__main__' == __name__:
   
-    polygon_shapefile = r"path\to\Polygon.shp"
-    POLYGON_ID_FEILD = 'Polygon_ID'
+    POLYGON_SHAPEFILE = r"path\to\Polygon.shp"
+    POLYGON_ID_FIELD = 'Polygon_UID'
     
-    output_folder = os.path.dirname(polygon_shapefile)
-    output_point_name = 'Points_' + os.path.basename(polygon_shapefile)
+    output_folder = os.path.dirname(POLYGON_SHAPEFILE)
+    output_point_name = 'Points_' + os.path.basename(POLYGON_SHAPEFILE)
     output_point_path = os.path.join(output_folder, output_point_name)
 
-    if polygon_shapefile.lower().endswith('shp'):
-        polygon_gpd = gpd.read_file(polygon_shapefile)
-        all_points_gdf = get_polygon_points(polygon_gpd)
+    if POLYGON_SHAPEFILE.lower().endswith('shp'):
+        polygon_gdf = gpd.read_file(POLYGON_SHAPEFILE)
+        all_points_gdf = get_polygon_points(polygon_gdf)
         if not all_points_gdf.empty:
             all_points_gdf.to_file(output_point_path, driver='ESRI Shapefile')
     else:
-        print(f"{polygon_shapefile} is not a ESRI Shapefile.")
+        print(f"{POLYGON_SHAPEFILE} is not a ESRI Shapefile.")
